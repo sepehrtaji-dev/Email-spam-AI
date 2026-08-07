006a56c4b69d98db64982b5e90a3051f910e7e99
Sepehr <sepehrbizixer@gmail.com>
2026-08-08 00:48:29 +0330

    imported requirements

---
+++ model.py
@@ -0,0 +1,7 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import torch.nn as nn
+import torch
+from torch.utils.data import DataLoader, Dataset
+import pandas as pd
+import matplotlib.pyplot as plt
