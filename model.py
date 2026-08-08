8d2a8ec445b281f1b1b9a8a065c4a2b78c6e64df
Sepehr <sepehrbizixer@gmail.com>
2026-08-08 12:25:04 +0330

    data prepared

--- model.py
+++ model.py
@@ -2,11 +2,23 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import torch.nn as nn
 import torch
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, TensorDataset
 import pandas as pd
 import matplotlib.pyplot as plt
-
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"device : {torch.cuda.get_device_name(device)}")
 df = pd.read_csv("spambase_csv.csv")
 x = df.drop("class", axis=1)
 y = df["class"]
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2, random_state=42)
+x_scaler = StandardScaler()
+x_train_scaled = x_scaler.fit_transform(x_train)
+x_test_scaled = x_scaler.transform(x_test)
+x_train_tensor = torch.tensor(x_train_scaled, dtype=torch.float32, device=device)
+x_test_tensor = torch.tensor(x_test_scaled, dtype=torch.float32, device=device)
+y_train_tensor = torch.tensor(y_train.values.reshape(-1, 1), dtype=torch.float32, device=device)
+y_test_tensor = torch.tensor(y_test.values.reshape(-1, 1), dtype=torch.float32, device=device)
+train_ds = TensorDataset(x_train_tensor, y_train_tensor)
+test_ds = TensorDataset(x_test_tensor, y_test_tensor)
+train_loader = DataLoader(train_ds, batch_size=128, shuffle=True)
+test_loader = DataLoader(test_ds, batch_size=128, shuffle=False)
