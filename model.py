b5a2d91b5116decf7ec3e09142a8644dbef4f1a1
Sepehr <sepehrbizixer@gmail.com>
2026-08-08 13:09:27 +0330

    bug fixed

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import torch
 from torch.utils.data import DataLoader, TensorDataset
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"device : {torch.cuda.get_device_name(device)}")
 df = pd.read_csv("spambase_csv.csv")
@@ -20,32 +21,27 @@ y_train_tensor = torch.tensor(y_train.values.reshape(-1, 1), dtype=torch.float32
 y_test_tensor = torch.tensor(y_test.values.reshape(-1, 1), dtype=torch.float32, device=device)
 train_ds = TensorDataset(x_train_tensor, y_train_tensor)
 test_ds = TensorDataset(x_test_tensor, y_test_tensor)
-train_loader = DataLoader(train_ds, batch_size=128, shuffle=True)
-test_loader = DataLoader(test_ds, batch_size=128, shuffle=False)
+train_loader = DataLoader(train_ds, batch_size=256, shuffle=True)
+test_loader = DataLoader(test_ds, batch_size=25, shuffle=False)
 
 class ClassficModel(nn.Module):
     def __init__(self, in_feature):
         super().__init__()
-        self.fc1 = nn.Linear(in_feature, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 64)
-        self.fc4 = nn.Linear(64, 32)
-        self.fc5 = nn.Linear(32, 16)
-        self.fc6 = nn.Linear(16, 1)
+        self.fc1 = nn.Linear(in_feature, 16)
+        self.fc2 = nn.Linear(16, 8)
+        self.fc3 = nn.Linear(8, 1)
         self.relu = nn.ReLU()
     def forward(self, x):
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
-        x = self.relu(self.fc3(x))
-        x = self.relu(self.fc4(x))
-        x = self.relu(self.fc5(x))
-        x = self.fc6(x)
+        x = self.fc3(x)
         return x
 
-epochs = 100
+epochs = 130
 train_losses = []
 eval_losses = []
 model = ClassficModel(x_train_tensor.shape[1])
+model.to(device)
 cirtersion = nn.BCEWithLogitsLoss()
 optim = torch.optim.Adam(model.parameters(), lr=.001)
 
@@ -60,7 +56,7 @@ for epoch in range(epochs):
         optim.zero_grad()
         loss.backward()
         optim.step()
-        train_loss_total += loss
+        train_loss_total += loss.item()
     train_loss_total /= len(train_loader)
     train_losses.append(train_loss_total)
     model.eval()
@@ -71,10 +67,25 @@ for epoch in range(epochs):
             batch_y = batch_y.to(device)
             y_pred = model(batch_x)
             loss = cirtersion(y_pred, batch_y)
-            test_loss_total += loss
+            test_loss_total += loss.item()
         test_loss_total /= len(test_loader)
         eval_losses.append(test_loss_total)
-
+    if epoch % 10 == 0:
+        print(f"epoch : {epoch}, train loss : {train_loss_total}, test loss : {test_loss_total}")
+model.eval()
+with torch.no_grad():
+    # پیش‌بینی روی داده‌های تست
+    test_outputs = model(x_test_tensor)  # خروجی خام (logits)
+    
+    # تبدیل logits به احتمال با سیگموید، سپس تبدیل به 0 یا 1
+    predictions = torch.sigmoid(test_outputs)  # احتمال بین 0 تا 1
+    predicted_labels = (predictions >= 0.5).float()  # آستانه 0.5
+    
+    # محاسبه دقت (تعداد درست‌ها تقسیم بر کل)
+    correct = (predicted_labels == y_test_tensor).float().sum()
+    accuracy = (correct / len(y_test_tensor)) * 100
+    print(f"Correct : {correct}")
+    print(f"accuracy : {accuracy}")
 plt.plot(range(epochs), train_losses, label="train")
 plt.plot(range(epochs), eval_losses, label="test")
 plt.title("train and test losses")
