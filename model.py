ef0e3063039f1091bdd0d893f6b0108744a36c7b
Sepehr <sepehrbizixer@gmail.com>
2026-08-08 12:31:11 +0330

    model class created

--- model.py
+++ model.py
@@ -22,3 +22,22 @@ train_ds = TensorDataset(x_train_tensor, y_train_tensor)
 test_ds = TensorDataset(x_test_tensor, y_test_tensor)
 train_loader = DataLoader(train_ds, batch_size=128, shuffle=True)
 test_loader = DataLoader(test_ds, batch_size=128, shuffle=False)
+
+class ClassficModel(nn.Module):
+    def __init__(self, in_feature):
+        super().__init__()
+        self.fc1 = nn.Linear(in_feature, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 32)
+        self.fc5 = nn.Linear(32, 16)
+        self.fc6 = nn.Linear(16, 1)
+        self.relu = nn.ReLU()
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.relu(self.fc3(x))
+        x = self.relu(self.fc4(x))
+        x = self.relu(self.fc5(x))
+        x = self.fc6(x)
+        return x
