cb046e7e40b54b408d174755691d7b90e67d65c3
Sepehr <sepehrbizixer@gmail.com>
2026-08-08 12:07:33 +0330

    data extracted and splited

--- model.py
+++ model.py
@@ -5,3 +5,8 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 import pandas as pd
 import matplotlib.pyplot as plt
+
+df = pd.read_csv("spambase_csv.csv")
+x = df.drop("class", axis=1)
+y = df["class"]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2, random_state=42)
